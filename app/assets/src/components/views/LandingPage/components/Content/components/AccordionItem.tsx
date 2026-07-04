86bdeaee54038fcc693d8fdbc3bb925c16ace175
Ikram Ahmed <ikrama@slower.ai>
2026-07-03 22:59:14 -0500

    REBRAND-13: Fix CZI blue leaking into open FAQ accordion state

--- app/assets/src/components/views/LandingPage/components/Content/components/AccordionItem.tsx
+++ app/assets/src/components/views/LandingPage/components/Content/components/AccordionItem.tsx
@@ -27,7 +27,12 @@ const AccordionItem = (props: AccordionItemProps) => {
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <circle cx="9.80701" cy="10.2426" r="9.80701" fill="#3867FA" />
+          <circle
+            className={cs.openCircle}
+            cx="9.80701"
+            cy="10.2426"
+            r="9.80701"
+          />
           <path d="M5.00977 10.2422L14.6045 10.2422" stroke="white" />
         </svg>
       ) : (
@@ -59,9 +64,7 @@ const AccordionItem = (props: AccordionItemProps) => {
         </svg>
       )}
       <span
-        className={cs.accordionTitle}
-        // @ts-expect-error CZID-8698 expect strictNullCheck error: error TS2322
-        style={props.isOpen ? { color: "#3867FA" } : null}
+        className={`${cs.accordionTitle}${props.isOpen ? ` ${cs.open}` : ""}`}
       >
         {props.accordionTitle}
       </span>
